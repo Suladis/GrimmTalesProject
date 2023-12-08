6b1db5fe433c3162d079f2be88081c7727f296c1
SuleAdis <115189123+Suladis@users.noreply.github.com>
2023-12-07 19:37:10 -0500

    Working Test for Discussion

--- disc.js
+++ disc.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
     $.ajax({
-        type: "Get",
+        type: "GET",
         url: "formData.json",
         dataType: "json",
         success: function(data) {
+            data.formData.forEach(function (entry) {
+                var html = '<div>';
+                html += '<p>Email: ' + entry.email + '</p>';
+                html += '<p>Story Name: ' + entry.storyName + '</p>';
+                html += '<p>Comments: ' + entry.comments + '</p>';
+                html += '</div><hr>';
+                
+                // Append the data to the container
+                $('.container').append(html);
+              });
     
         },
         error: function(){
             alert("json not found");
         }
     });
+
 });
